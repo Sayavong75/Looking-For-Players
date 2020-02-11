1ea0fb209d75e84c396160ac8e6827477fed951c
Guillaume1979 <guibetguillaume@gmail.com>
2020-02-11 17:43:02 +0100

    ajout du front

---
+++ Front/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import {RouterModule} from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PlayerProfileComponent } from './player-profile/player-profile.component';
+import { NewSessionComponent } from './new-session/new-session.component';
+import { SessionListComponent } from './sessions-list/session-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    NavbarComponent,
+    HomeComponent,
+    PlayerProfileComponent,
+    NewSessionComponent,
+    SessionListComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '' , component: HomeComponent },
+      {path: 'playerprofile' , component: PlayerProfileComponent},
+      {path: 'newsession' , component: NewSessionComponent},
+      {path: 'sessionsList' , component: SessionListComponent}
+        ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Front/src/app/new-session/new-session.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-session',
+  templateUrl: './new-session.component.html',
+  styleUrls: ['./new-session.component.css']
+})
+export class NewSessionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
