00b99d1101d48254c06262c4f1c6dc1d2035d27e
Guillaume1979 <guibetguillaume@gmail.com>
2020-02-12 17:26:42 +0100

    premier commit back

---
+++ Back/src/main/java/co/simplon/lfpapi/LookingForPlayersApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.lfpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingForPlayersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookingForPlayersApplication.class, args);
+    }
+
+}
---
+++ Back/src/test/java/co/simplon/lfpapi/LookingForPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.lfpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingForPlayersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
