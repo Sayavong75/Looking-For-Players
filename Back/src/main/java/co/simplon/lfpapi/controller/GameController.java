cd3d94f6fa18d9cc27d4f8f60e639ba4f2931a1e
Sayavong <sachaevans75@gmail.com>
2020-03-02 14:56:27 +0100

    modify GameController

--- Back/src/main/java/co/simplon/lfpapi/controller/GameController.java
+++ Back/src/main/java/co/simplon/lfpapi/controller/GameController.java
@@ -2,10 +2,8 @@ package co.simplon.lfpapi.controller;
 
 import co.simplon.lfpapi.model.Game;
 import co.simplon.lfpapi.service.GameService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,12 @@ public class GameController {
     @GetMapping
     public List<Game> getGames() {
         return gameService.getGames();
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> addGame(@RequestBody Game gameToAdd) {
+        Game savedGame = gameService.addGame(gameToAdd);
+        return ResponseEntity.ok(savedGame);
     }
 
 }
