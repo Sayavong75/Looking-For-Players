01c97f8301a521f57db695f7f94dab5ba2e16ce2
Guillaume1979 <guibetguillaume@gmail.com>
2020-03-03 17:02:31 +0100

    suppression du Swagger UI qui empeche la compilation

--- Back/src/main/java/co/simplon/lfpapi/LookingForPlayersApplication.java
+++ Back/src/main/java/co/simplon/lfpapi/LookingForPlayersApplication.java
@@ -2,10 +2,10 @@ package co.simplon.lfpapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-@EnableSwagger2
+
 public class LookingForPlayersApplication {
 
     public static void main(String[] args) {
