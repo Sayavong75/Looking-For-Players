c7d1346bf369eb9fec7fb366ca441e1a7cba70d4
Guillaume1979 <guibetguillaume@gmail.com>
2020-03-27 17:54:38 +0100

    ajout de l'annotation cross origin sur les controllers

--- Back/src/main/java/co/simplon/lfpapi/controller/SessionController.java
+++ Back/src/main/java/co/simplon/lfpapi/controller/SessionController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sessions")
+@CrossOrigin("http://localhost:4200")
 public class SessionController {
 
     private SessionService sessionService;
