9659a430825f2caa789bdf18e35986c901e815bc
Sayavong SOUANDARAVONG <sachaevans75@gmail.com>
2020-03-29 21:45:32 +0200

    Front modification player add et new session

--- Front/src/app/new-session/new-session.component.ts
+++ Front/src/app/new-session/new-session.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Player} from '../class/player';
+import {Game} from '../class/game';
+import {ApiConnectionService} from '../service/api-connection.service';
+import {FormBuilder} from '@angular/forms';
+import {Favorite} from '../class/favorite';
+import {Session} from '../class/session';
 
 @Component({
   selector: 'app-new-session',
@@ -6,10 +12,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-session.component.css']
 })
 export class NewSessionComponent implements OnInit {
+  theDate: any;
+  sessionForm;
+  session: Session;
+  game: Game;
+  games: Game[];
+  player: Player;
+  players: Player[];
+  favorite: Favorite;
+  favorites: Favorite[];
+  gameName;
+  public minDate: Date = new Date(Date.now());
+  public maxDate: Date = new Date ('12/31/2020');
+  public value: Date = new Date (Date.now());
+  usernameValue: string = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private connectionService: ApiConnectionService, private formBuilder: FormBuilder) {
+    this.sessionForm = this.formBuilder.group({
+      username: '',
+      gameName: '',
+      dateSession: ''
+    });
   }
 
+  ngOnInit() {
+    this.connectionService.getGames().subscribe(
+      games => this.games = games
+    );
+  }
+
+  onSubmit(session) {
+    console.log(session);
+  }
 }
--- Front/src/app/player-add/player-add.component.ts
+++ Front/src/app/player-add/player-add.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiConnectionService} from '../service/api-connection.service';
-import { FormBuilder } from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Player} from "../class/player";
 
 @Component({
   selector: 'app-player-add',
@@ -15,21 +16,30 @@ export class PlayerAddComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private connectionService: ApiConnectionService
-    ) {
-      this.playerForm = this.formBuilder.group({
-        username: ''
-      });
+  ) {
+    this.playerForm = this.formBuilder.group({
+      username: ''
+    });
   }
-    onSubmit(player) {
-      this.txtValue = player.username;
-      if (this.txtValue == null) {
-        window.alert('Tu as oublié d\'indiquer ton pseudo de player !');
-      } else {
-        this.connectionService.addPlayer(player).subscribe(savePlayer => console.log(savePlayer));
-        this.playerForm.reset();
-        window.alert('Player ' + player.username + ' a été ajouté(e)');
-      }
+
+  onSubmit(player) {
+    console.log('validation des données new player');
+    this.txtValue = player.username;
+    if (this.txtValue === null) {
+      window.alert('Tu as oublié d\'indiquer ton pseudo de joueur !');
+    } else {
+      this.connectionService.addPlayer(player).subscribe(
+        savePlayer => {
+          console.log(savePlayer);
+          this.playerForm.reset();
+          window.alert('Joueur ' + player.username + ' a été ajouté');
+        }, error => {
+          console.log('erreur de mise à jour :');
+          console.log(error);
+        }
+      );
     }
+  }
 
   ngOnInit() {
   }
